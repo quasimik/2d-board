94e1bbb877da16a00abf390a8928f6f40f76777a
Michael Liu <mike666234@gmail.com>
2017-04-05 18:36:20 -0700

    Add Board

---
+++ index.js
@@ -0,0 +1,26 @@
+function Board(x, y) {
+  if (arguments.length !== 2) {
+    throw new Error("Board::Must supply two numbers as parameters.")
+  }
+  if (typeof x !== "number" || x !== x) {
+    throw new TypeError("Board()::parameter 1 must be a number.");
+  }
+  if (typeof y !== "number" || y !== y) {
+    throw new TypeError("Board()::parameter 2 must be a number.");
+  }
+  this._x = x;
+  this._y = y;
+  return this;
+}
+
+Board.prototype.x = function() {
+  return this._x;
+};
+
+Board.prototype.y = function() {
+  return this._y;
+};
+
+exports.print = function() {
+  return new Board(x, y);
+}
